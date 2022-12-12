7b1e8af3fbf72fd3f97aeb6d6741746f3d49b208
ankit rana <105877301+ankitrana2709@users.noreply.github.com>
2022-12-12 16:21:43 +0530

    ok

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # You need to generate password from gmail< settings< security< pass for app< mail.
-PASSWORD=""
-EMAIL="yourmail@gmail.com"
+PASSWORD="rytjlsurraiaysys"
+EMAIL="rana1997ankit23@gmail.com"
